733f99c95a0691f0387c8af369b028a71b4d0c9e
dk7648 <dk7648@korea.ac.kr>
2024-01-16 15:50:52 +0900

    feat: rest api 추가

---
+++ src/main/java/com/example/firstproject/api/ArticleApiController.java
@@ -0,0 +1,64 @@
+package com.example.firstproject.api;
+
+import com.example.firstproject.dto.ArticleForm;
+import com.example.firstproject.entity.Article;
+import com.example.firstproject.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+public class ArticleApiController {
+    @Autowired
+    private ArticleRepository articleRepository;
+
+    //GET
+    @GetMapping("/api/articles")
+    public List<Article> index() {
+        return articleRepository.findAll();
+    }
+
+    @GetMapping("/api/articles/{id}")
+    public Article show(@PathVariable Long id) {
+        return articleRepository.findById(id).orElse(null);
+    }
+
+    //POST
+    @PostMapping("/api/articles")
+    public Article create(@RequestBody ArticleForm dto) {
+        Article article = dto.toEntity();
+        return articleRepository.save(article);
+    }
+
+    //PATCH
+    @PatchMapping("/api/articles/{id}")
+    public ResponseEntity<Article> update(@PathVariable Long id, @RequestBody ArticleForm dto) {
+        Article article = dto.toEntity();
+        Article target = articleRepository.findById(id).orElse(null);
+        if (target == null || id != article.getId()) {
+            log.info("잘못된 요청! id: {}, article: {}", id, article.toString());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+        target.patch(article);
+        Article updated = articleRepository.save(target);
+        return ResponseEntity.status(HttpStatus.OK).body(updated);
+    }
+
+    //DELETE
+    @DeleteMapping("/api/articles/{id}")
+    public ResponseEntity<Article> delete(@PathVariable Long id) {
+        Article target = articleRepository.findById(id).orElse(null);
+        if (target == null) {
+            log.info("잘못된 요청!");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+        articleRepository.delete(target);
+        return ResponseEntity.status(HttpStatus.OK).body(null); //body(null) == build()
+
+    }
+}
--- src/main/java/com/example/firstproject/entity/Article.java
+++ src/main/java/com/example/firstproject/entity/Article.java
@@ -2,10 +2,7 @@ package com.example.firstproject.entity;
 
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
 
 @AllArgsConstructor
 @NoArgsConstructor //기본 생성자 추가 어노테이션
@@ -21,5 +18,10 @@ public class Article {
     @Column
     private String content;
 
-
+    public void patch(Article article) {
+        if(article.title != null)
+            this.title = article.title;
+        if(article.content != null)
+            this.content = article.content;
+    }
 }
