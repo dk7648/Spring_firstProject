f4c7e78ccd6605ac8b51480d58389462b8a092a9
dk7648 <dk7648@korea.ac.kr>
2024-01-18 13:51:58 +0900

    feat: 트랜잭션 맛보기 코드 추가

--- src/main/java/com/example/firstproject/service/ArticleService.java
+++ src/main/java/com/example/firstproject/service/ArticleService.java
@@ -3,6 +3,7 @@ package com.example.firstproject.service;
 import com.example.firstproject.dto.ArticleForm;
 import com.example.firstproject.entity.Article;
 import com.example.firstproject.repository.ArticleRepository;
+import jakarta.transaction.Transactional;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -10,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -52,5 +54,21 @@ public class ArticleService {
         }
         articleRepository.delete(target);
         return target;
+    }
+
+    @Transactional
+    public List<Article> createArticles(List<ArticleForm> dtos) {
+        //1. dto 묶음을 엔티티 묶음으로 변환하기
+        List<Article> articleList = dtos.stream()
+                .map(dto -> dto.toEntity())
+                .collect(Collectors.toList());
+        //2. 엔티티 묶음을 DB에 저장하기
+        articleList.stream()
+                .forEach(article -> articleRepository.save(article));
+        //3. 강제 예외 발생시키기
+        articleRepository.findById(-1L)
+                .orElseThrow(() -> new IllegalArgumentException("결제 실패!"));
+        //4. 결과 값 반환하기
+        return articleList;
     }
 }
