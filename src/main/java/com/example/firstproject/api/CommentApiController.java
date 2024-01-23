edb051b4acb0c4e0bdca7c6c2880b763494b37b7
dk7648 <dk7648@korea.ac.kr>
2024-01-23 19:46:37 +0900

    feat: Comment 조회 기능 추가

---
+++ src/main/java/com/example/firstproject/api/CommentApiController.java
@@ -0,0 +1,32 @@
+package com.example.firstproject.api;
+
+import com.example.firstproject.dto.CommentDto;
+import com.example.firstproject.service.CommentService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+public class CommentApiController {
+    @Autowired
+    private CommentService commentService;
+    // 1. 댓글 조회
+    @GetMapping("/api/articles/{articleId}/comments")
+    public ResponseEntity<List<CommentDto>> comments(@PathVariable Long articleId) {
+        // 서비스에 위임
+        List<CommentDto> dtos = commentService.comments(articleId);
+        // 결과 응답
+        return ResponseEntity.status(HttpStatus.OK).body(dtos);
+    }
+    // 2. 댓글 생성
+    // 3. 댓글 수정
+    // 4. 댓글 삭제
+
+}
